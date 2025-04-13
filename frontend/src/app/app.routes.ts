27aed2bf1f7154e7502187ce93605ff85ebb7554
zNahuelz <102327040+zNahuelz@users.noreply.github.com>
2025-04-13 14:31:26 -0500

    [F] Testing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LoginComponent} from './components/shared/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LoginComponent, title: 'ALTERNATIVA CASANOVA - INICIO DE SESIÓN'},
+];
